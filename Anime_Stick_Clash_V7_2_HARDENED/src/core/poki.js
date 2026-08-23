2eaac18ac46cf54711a6bd2a488801f2d10bb8fc
haxuanlam734-bit <haxuanlam734@gmail.com>
2026-08-23 10:25:53 +0700

    Delete Anime_Stick_Clash_V7_2_HARDENED directory

--- Anime_Stick_Clash_V7_2_HARDENED/src/core/poki.js
+++
@@ -1,8 +0,0 @@
-export const Poki={
-async init(){try{if(window.PokiSDK?.init)await window.PokiSDK.init()}catch(e){console.warn("Poki init unavailable",e)}},
-load(){try{window.PokiSDK?.gameLoadingFinished?.()}catch(e){}},
-start(){try{window.PokiSDK?.gameplayStart?.()}catch(e){}},
-stop(){try{window.PokiSDK?.gameplayStop?.()}catch(e){}},
-async ad(){try{if(window.PokiSDK?.commercialBreak)await window.PokiSDK.commercialBreak()}catch(e){}},
-async reward(){try{if(!window.PokiSDK?.rewardedBreak)return true;let rewarded=false;await window.PokiSDK.rewardedBreak(()=>rewarded=true);return rewarded}catch(e){return true}}
-};
